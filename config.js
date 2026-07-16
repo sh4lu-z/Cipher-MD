a3995a24207bed18646051d219142e041ec10cd2
Shaluka Gimhan <201992093+sh4lu-z@users.noreply.github.com>
2026-07-16 22:32:06 +0530

    refactor: update dotenv configuration and remove deprecated start script; add bootstrap script for core installation

--- config.js
+++ config.js
@@ -1,21 +1,13 @@
 const fs = require('fs');
-require('dotenv').config();
+require('dotenv').config({ quiet: true });
 
 module.exports = {
-
-    // 👑 GHOST OWNER
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "",
-    OWNER_PHONE: process.env.OWNER_PHONE || "",
-    PAIRING_NUMBER: process.env.PAIRING_NUMBER || "",
-
 
     BOT_NAME: process.env.BOT_NAME || "Cipher",
 
     CHANNEL_LINK: "https://whatsapp.com/channel/0029Vb6o3EiGJP8CoA3Olr28",
 
     FOOTER: "\n\n> ©︎ ᴄɪᴘʜᴇʀ ᴍᴅ ⚡︎",
-
-    MONGO_URL: process.env.MONGO_URL || '',
 
     MENU_IMAGE: process.env.MENU_IMAGE || "https://i.ibb.co/Fqkx25c5/balloon-Minimalist-Desktop-Wallpaper-1.png",
 
