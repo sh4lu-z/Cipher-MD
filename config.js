c80d6650e658649d4e0ba8f752137e6bf069604f
sh4lu-z <hello@shalukagimhan.com>
2026-09-17 17:36:06 +0000

    Update from https://github.com/sh4lu-z/Cipher_MD/commit/26d553d20af39c51f45468b964d4febd009b39ac

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
 
     CHANNEL_LINK: "https://whatsapp.com/channel/0029Vb6o3EiGJP8CoA3Olr28",
 
-    FOOTER: "\n\n> ©︎ ᴄɪᴘʜᴇʀ ᴍᴅ ⚡︎",
+    FOOTER: "\n\n> ©︎ ᴄɪᴘʜᴇʀ ᴍᴅ > s4z.top",
 
     MENU_IMAGE: process.env.MENU_IMAGE || "https://i.ibb.co/Fqkx25c5/balloon-Minimalist-Desktop-Wallpaper-1.png",
 
